fbd21c4be529f8e9a54e4afb0ec13e9a81474255
조신영(mac) <>
2022-10-04 20:13:50 +0900

    refactor: call-by-referenced radix sort

--- HW2/code/radix_sort.cc
+++ HW2/code/radix_sort.cc
@@ -1,29 +1,29 @@
 #include <vector>
 using namespace std;
 
-vector<__int128> counting_sort_10(vector<__int128> arr, __int128 exp) {
+void counting_sort_10(vector<__int128>& arr, __int128 exp) {
   vector<vector<__int128>> counting_arr(10);
-  vector<__int128> sorted_arr;
 
   for (__int128 cur : arr) {
     __int128 cur_digit = (cur / exp) % 10;
     counting_arr[cur_digit].push_back(cur);
   }
 
-  for (int i = 0; i < 10; i++) {
-    sorted_arr.insert(sorted_arr.end(), counting_arr[i].begin(),
-                      counting_arr[i].end());
-  }
+  int start = 0;
+  for (vector<__int128> cur_arr : counting_arr) {
+    for (int i = 0; i < cur_arr.size(); i++) {
+      arr[start + i] = cur_arr[i];
+    }
 
-  return sorted_arr;
+    start += cur_arr.size();
+  }
 }
 
 vector<__int128> radix_sort(vector<__int128> arr) {
   __int128 max_val = *max_element(arr.begin(), arr.end());
-  vector<__int128> sorted_arr = arr;
 
   for (__int128 exp = 1; max_val / exp > 0; exp *= 10)
-    sorted_arr = counting_sort_10(sorted_arr, exp);
+    counting_sort_10(arr, exp);
 
-  return sorted_arr;
+  return arr;
 }
