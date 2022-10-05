968456392fed20416425e9c5911e6bfd2a5049b6
조신영(mac) <>
2022-10-06 00:54:42 +0900

    refactor: insertion sort logic

--- HW2/code/insertion_sort.cc
+++ HW2/code/insertion_sort.cc
@@ -3,25 +3,15 @@
 using namespace std;
 
 vector<__int128> insertion_sort(vector<__int128> arr) {
-  vector<__int128> sorted_arr;
-  sorted_arr.push_back(arr[0]);
-
   for (int i = 1; i < arr.size(); i++) {
     __int128 cur = arr[i];
-    bool inserted = false;
 
-    for (int j = 0; j < sorted_arr.size(); j++) {
-      if (compare(cur, sorted_arr[j])) {
-        sorted_arr.insert(sorted_arr.begin() + j, cur);
-        inserted = true;
-        break;
-      }
-    }
-
-    if (!inserted) {
-      sorted_arr.push_back(cur);
+    int j = i - 1;
+    while (j >= 0 && !compare(arr[j], cur)) {
+      swap(arr[j + 1], arr[j]);
+      j--;
     }
   }
 
-  return sorted_arr;
+  return arr;
 }
