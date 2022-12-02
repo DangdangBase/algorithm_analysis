5f0bd65191704d8d4c9b205850bd8917a6c69708
조신영(mac) <>
2022-12-02 18:43:21 +0900

    refactor: multi-threading, ll to int

--- HW4/apsp.cc
+++ HW4/apsp.cc
@@ -5,24 +5,23 @@
 #include <thread>
 #include <vector>
 #define ll long long
-#define P pair<ll, int>
-#define INF 9223372036854775807
+#define P pair<int, int>
+#define INF 987654321
 using namespace std;
 
 vector<vector<P>> graph;
 int u, v, w;
 int vertex_num;
-ll max_len = 0;
+int max_len = 0;
 int start_v, end_v;
 
 int vertex_opt[3] = {16000, 32000, 1000000};
 time_t start_time, end_time;
-thread threads[100];
 mutex mtx;
 
 void dijkstras(int start) {
   priority_queue<P, vector<P>, greater<P>> pq;
-  vector<ll> dist(vertex_num, INF);
+  vector<int> dist(vertex_num, INF);
 
   pq.push({0, start});
   dist[start] = 0;
@@ -42,10 +41,10 @@ void dijkstras(int start) {
     }
   }
 
-  ll local_max_len = 0;
+  int local_max_len = 0;
   int local_end_v;
   for (int i = 0; i < vertex_num; i++) {
-    if (dist[i] == INF && dist[i] > local_max_len) {
+    if (dist[i] != INF && dist[i] > local_max_len) {
       local_max_len = dist[i];
       local_end_v = i;
     }
@@ -76,6 +75,9 @@ void print_elapsed_time() {
 }
 
 int main(int argc, char* argv[]) {
+  int max_thread_num = thread::hardware_concurrency();
+  thread threads[max_thread_num];
+
   vertex_num = vertex_opt[stoi(argv[1])];
   ifstream input("./data/" + to_string(vertex_num) + ".graph");
 
@@ -94,20 +96,28 @@ int main(int argc, char* argv[]) {
   }
 
   start_time = time(NULL);
-  for (int i = 0; i < vertex_num / 100; i++) {
-    for (int j = 0; j < 100; j++) {
-      threads[j] = thread{dijkstras, i * 100 + j};
+  for (int i = 0; i < vertex_num; i++) {
+    int th_idx = i % max_thread_num;
+
+    if (threads[th_idx].joinable()) {
+      threads[th_idx].join();
     }
 
-    for (int j = 0; j < 100; j++) {
-      threads[j].join();
-    }
+    threads[th_idx] = thread{dijkstras, i};
 
-    printf("%d / %d\n", i * 100, vertex_num);
-    print_elapsed_time();
-    cout << "\n";
+    if (!(i % 100)) {
+      printf("%d / %d\n", i, vertex_num);
+      print_elapsed_time();
+      cout << "\n";
+    }
   }
 
-  cout << max_len << endl;
+  for (int i = 0; i < 10; i++) {
+    if (threads[i].joinable()) {
+      threads[i].join();
+    }
+  }
+
+  printf("start_v: %d, end_v: %d, max_len: %d\n", start_v, end_v, max_len);
   return 0;
 }
